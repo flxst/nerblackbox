55ead65fc197d344d14dbc128f876509f11fb7c6
Felix Stollenwerk <felix.stollenwerk@arbetsformedlingen.se>
2020-01-17 17:30:58 +0100

    restructure NER training

--- utils/ner_trainer.py
+++ utils/ner_trainer.py
@@ -21,65 +21,107 @@ class NERTrainer(object):
                  valid_dataloader,
                  label_list,
                  fp16=False):
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        """
+        :param model:            [transformers BertForTokenClassification]
+        :param train_dataloader: [pytorch DataLoader]
+        :param valid_dataloader: [pytorch DataLoader]
+        :param label_list:       [list] of [str]
+        :param fp16:             [bool]
+        """
+        # input attributes
         self.model = model
-        self.model.to(self.device)
-        # if fp16: self.model.half()
-
-        self.writer = SummaryWriter()
-        
-        self.loss_fct = CrossEntropyLoss(ignore_index=0)  # NEW: HERE 3
-        # self.loss_fct = CrossEntropyLoss()  # OLD: HERE 3
         self.train_dataloader = train_dataloader
         self.valid_dataloader = valid_dataloader
         self.label_list = label_list
         self.fp16 = fp16
 
+        # derived attributes
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model.to(self.device)
+        # if fp16:
+        #     self.model.half()
+
+        self.writer = SummaryWriter()
+
+        """
+        self.loss_fct = CrossEntropyLoss(ignore_index=0)  # NEW: HERE 3
+        # self.loss_fct = CrossEntropyLoss()  # OLD: HERE 3
+        """
+
         # no input arguments
-        self.num_epochs = None
+        # learning rate
         self.learning_rate = None
         self.warmup_proportion = None
         self.optimizer = None
+
         self.total_steps = None
-        self.accuracy_hist = None
-        self.f1_score_hist = None
-        self.loss_hist = np.array([])
-        self.val_f1_score_hist = None
-        self.val_accuracy_hist = None
-        self.val_loss_hist = None
+
+        # metrics
+        self.metrics = {
+            'batch': {
+                'train': {
+                    'loss': list(),
+                    'acc': list(),
+                    'f1': list(),
+                    'lr': list(),
+                },
+                'valid': {
+                    'loss': list(),
+                    'acc': list(),
+                    'f1': list(),
+                },
+            },
+            'epoch': {
+                'valid': {
+                    'loss': list(),
+                    'acc': list(),
+                    'f1': list(),
+                },
+            },
+        }
         self.labelDict = None
 
     ####################################################################################################################
     # 1. FIT & VALIDATION
     ####################################################################################################################
-    def fit(self, num_epochs=25, max_grad_norm=2.0, learning_rate=3e-5, warmup_proportion=0.1):
-        self.num_epochs = num_epochs
+    def fit(self,
+            num_epochs=25,
+            max_grad_norm=2.0,
+            learning_rate=3e-5,
+            warmup_proportion=0.1,
+            verbose=False):
+
+        # learning rate
         self.learning_rate = learning_rate
         self.warmup_proportion = warmup_proportion
-        
-        self.optimizer = self.create_optimizer(self.fp16)
+
+        self.optimizer = self.create_optimizer(self.learning_rate, self.fp16)
         if self.device == "cpu":
             self.model, self.optimizer = amp.initialize(self.model, self.optimizer, opt_level='O1')
-        self.total_steps = self.get_total_steps()
-        
+
+        self.total_steps = self.get_total_steps(num_epochs)
+
+        # metrics
+        """
         self.accuracy_hist = np.array([])
         self.f1_score_hist = np.array([])
         self.loss_hist = np.array([])
         self.val_f1_score_hist = np.array([])
         self.val_accuracy_hist = np.array([])
         self.val_loss_hist = np.array([])
-        
+        """
         self.labelDict = {
             k: {'total': 0, 'correct': 0}
             for k in self.label_list
         }
         
-        # epoch_process = master_bar(range(self.num_epochs))
+        # epoch_process = master_bar(range(num_epochs))
         # for epoch in epoch_process:
-        pbar = tqdm(total=self.num_epochs*len(self.train_dataloader))
+        pbar = tqdm(total=num_epochs*len(self.train_dataloader))
         global_step = None
         self.model.zero_grad()  ## HERE 2
-        for epoch in range(self.num_epochs):
+
+        for epoch in range(num_epochs):
             print()
             print(">>> Train Epoch: {}".format(epoch))
             self.model.train()
@@ -88,7 +130,9 @@ class NERTrainer(object):
             for step, batch in enumerate(self.train_dataloader):
                 batch = tuple(t.to(self.device) for t in batch)
                 input_ids, input_mask, segment_ids, label_ids = batch
-                if step == 0:
+
+                if verbose and step == 0:
+                    print('\n--- input tensors ---')
                     print('> input_ids[0]:', input_ids.shape)
                     print(input_ids[0])
                     print('> input_mask[0]:', input_mask.shape)
@@ -97,6 +141,7 @@ class NERTrainer(object):
                     print(segment_ids[0])
                     print('> label_ids[0]:', label_ids.shape)
                     print(label_ids[0])
+
                 """
                 logits = self.model(input_ids,
                                     attention_mask=input_mask,
@@ -116,52 +161,48 @@ class NERTrainer(object):
                                      )
                 loss, logits_tmp = outputs[:2]
                 logits = [logits_tmp]
-                print(f'loss: {loss}')
+                print(f'Batch #{step} train loss: {loss}')
 
-                accuracy = self.accuracy(logits, label_ids)
-                f1_score = self.f1_score_accuracy(logits, label_ids)
-                
-                self.accuracy_hist = np.append(self.accuracy_hist, accuracy)
-                self.f1_score_hist = np.append(self.f1_score_hist, f1_score)
-                self.loss_hist = np.append(self.loss_hist, loss.mean().item())
+                # metrics
+                acc = self.accuracy(logits, label_ids)
+                f1 = self.f1_score_accuracy(logits, label_ids)
+                self.metrics['batch']['train']['loss'].append(loss.mean().item())
+                self.metrics['batch']['train']['acc'].append(acc)
+                self.metrics['batch']['train']['f1'].append(f1)
+                pbar.update(1)  # TQDM - progressbar
+                pbar.set_description("Acc: " + str(round(acc, 5)) + " F1: " + str(round(f1, 5)))
 
+                # backpropagation
                 if self.fp16:
-                    # loss.backward()
-                    # self.optimizer.step()
-                    # self.optimizer.backward(loss)
                     with amp.scale_loss(loss, self.optimizer) as scaled_loss:
                         scaled_loss.backward()
                 else:
                     loss.backward()
-                
+
                 # TODO undersök varför man vill göra det här, det får ibland modellen att inte lära sig
                 # self.clip_grad_norm(max_grad_norm)
-                
+                self.optimizer.step()
+                self.model.zero_grad()
+
+                # update learning rate
                 global_step = self.get_global_step(epoch, step)
-                 
-                # epoch_process.child.comment = ("Train F1 score: {:.2}".format(self.f1_score_hist.mean()))
-                
                 lr_this_step = self.update_learning_rate(global_step)
-                
-                # TQDM - progressbar
-                pbar.update(1)
-                pbar.set_description("Acc: " + str(round(accuracy, 5)) + " F1: " + str(round(f1_score, 5)))
-                
-                self.writer.add_scalar('train/accuracy', accuracy, global_step)
-                self.writer.add_scalar('train/f1_score', f1_score, global_step)
+                print(f'        update learning rate: {lr_this_step}')
+                self.metrics['batch']['train']['lr'].append(lr_this_step)
+
+                # writer
+                self.writer.add_scalar('train/accuracy', acc, global_step)
+                self.writer.add_scalar('train/f1_score', f1, global_step)
                 self.writer.add_scalar('train/loss', loss.mean().item(), global_step)
                 self.writer.add_scalar('train/learning_rate', lr_this_step, global_step)
 
-                self.optimizer.step()
-                self.model.zero_grad()
-                
             self.validation(global_step, epoch)
                 
-    def validation(self, global_step, epoch):
+    def validation(self, global_step, epoch, verbose=False):
         print()
         print(">>> Valid Epoch: {}".format(epoch))
         self.model.eval()
-        eval_loss, eval_accuracy = 0, 0
+        epoch_valid_loss, epoch_valid_acc = 0, 0
         nb_eval_steps, nb_eval_examples = 0, 0
         predictions, true_labels = [], []
         
@@ -193,30 +234,28 @@ class NERTrainer(object):
                 logits = [logits_tmp]
                 # print(f'eval_loss: {tmp_eval_loss}')
 
-            f1_score = self.f1_score_accuracy(logits, b_labels)
-            # print("F1-Score (macro) sklearn: {}".format(f1_score))
+            f1 = self.f1_score_accuracy(logits, b_labels)
+            # print("F1-Score (macro) sklearn: {}".format(f1))
             
             logits = logits[0].detach().cpu().numpy()
             label_ids = b_labels.to('cpu').numpy()
             predictions.extend([list(p) for p in np.argmax(logits, axis=2)])
             true_labels.append(label_ids)
             
-            tmp_eval_accuracy = self.flat_accuracy(logits, label_ids)
+            tmp_eval_acc = self.flat_accuracy(logits, label_ids)
             
-            eval_loss += tmp_eval_loss.mean().item()
-            eval_accuracy += tmp_eval_accuracy
+            epoch_valid_loss += tmp_eval_loss.mean().item()
+            epoch_valid_acc += tmp_eval_acc
 
             nb_eval_examples += b_input_ids.size(0)
             nb_eval_steps += 1
-        
-            self.val_f1_score_hist = np.append(self.val_f1_score_hist, f1_score)
-            self.val_accuracy_hist = np.append(self.val_accuracy_hist, tmp_eval_accuracy)
-            self.val_loss_hist = np.append(self.val_loss_hist, tmp_eval_loss.mean().item())
-            
-        eval_loss = eval_loss/nb_eval_steps
-        eval_accuracy = eval_accuracy/nb_eval_steps
-        print("Validation loss: {}".format(eval_loss))
-        print("Validation Accuracy: {}".format(eval_accuracy))
+
+            self.metrics['batch']['valid']['loss'].append(tmp_eval_loss.mean().item())
+            self.metrics['batch']['valid']['acc'].append(tmp_eval_acc)
+            self.metrics['batch']['valid']['f1'].append(f1)
+
+        epoch_valid_loss = epoch_valid_loss/nb_eval_steps
+        epoch_valid_acc = epoch_valid_acc/nb_eval_steps
 
         pred_tags = [self.label_list[p_i] for p in predictions for p_i in p]
         valid_tags = [self.label_list[l_ii] for l in true_labels for l_i in l for l_ii in l_i]
@@ -235,17 +274,27 @@ class NERTrainer(object):
         pred_tags_enriched = [enrich(pred_tags[i], pred_tags[i-1] if i > 0 else None) for i in range(len(pred_tags))]
         valid_tags_enriched = [enrich(valid_tags[i], valid_tags[i-1] if i > 0 else None) for i in range(len(valid_tags))]
 
-        print(pred_tags[:10], len(pred_tags))
-        print(valid_tags[:10], len(valid_tags))
-        print(pred_tags_enriched[:10], len(pred_tags_enriched))
-        print(valid_tags_enriched[:10], len(valid_tags_enriched))
+        if verbose:
+            print('\n--- predicted and true labels ---')
+            print(pred_tags[:10], len(pred_tags))
+            print(valid_tags[:10], len(valid_tags))
+            print(pred_tags_enriched[:10], len(pred_tags_enriched))
+            print(valid_tags_enriched[:10], len(valid_tags_enriched))
 
         # (valid_tags, pred_tags) = self.stripTags(valid_tags, pred_tags)
-        f1_score = f1_score_sklearn(pred_tags_enriched, valid_tags_enriched, average='macro')
-        print("Validation F1-Score: {}".format(f1_score))
+        epoch_valid_f1 = f1_score_sklearn(pred_tags_enriched, valid_tags_enriched, average='macro')
+
+        self.metrics['epoch']['valid']['loss'].append(epoch_valid_loss)
+        self.metrics['epoch']['valid']['acc'].append(epoch_valid_acc)
+        self.metrics['epoch']['valid']['f1'].append(epoch_valid_f1)
+
+        print(f'Epoch #{epoch} valid loss: {epoch_valid_loss}')
+        print(f'Epoch #{epoch} valid acc:  {epoch_valid_acc}')
+        print(f'Epoch #{epoch} valid f1:   {epoch_valid_f1}')
 
         self.calculate_percentage_correct(pred_tags, valid_tags)
-        
+
+        print('\n--- chunk-based (seqeval) classification report ---')
         print(classification_report(valid_tags_enriched, pred_tags_enriched))
 
         for idx, tag in enumerate(valid_tags):
@@ -254,6 +303,7 @@ class NERTrainer(object):
                 if pred_tags[idx] == tag:
                     resLabelDict[tag]['correct'] += 1
 
+        print('\n--- token-based classification report ---')
         count = {'total': 0, 'correct': 0}
         for label in self.label_list:
             if label in resLabelDict.keys():
@@ -264,9 +314,10 @@ class NERTrainer(object):
             c = count[field]
             print(f'{field} = {c}')
 
-        self.writer.add_scalar('validation/loss', eval_loss, global_step)
-        self.writer.add_scalar('validation/accuracy', eval_accuracy, global_step)
-        self.writer.add_scalar('validation/f1_score', f1_score, global_step)
+        # writer
+        self.writer.add_scalar('validation/loss', epoch_valid_loss, global_step)
+        self.writer.add_scalar('validation/accuracy', epoch_valid_acc, global_step)
+        self.writer.add_scalar('validation/f1_score', epoch_valid_f1, global_step)
 
     ####################################################################################################################
     # 2. METRICS
@@ -324,13 +375,25 @@ class NERTrainer(object):
         pred_flat = np.argmax(np_logits, axis=2).flatten()
         labels_flat = np_label_ids.flatten()
         return np.sum(pred_flat == labels_flat) / len(labels_flat)
-    
+
+    ####################################################################################################################
+    # 3. STEPS
+    ####################################################################################################################
     def get_global_step(self, epoch, step):
         return epoch * len(self.train_dataloader) + step
     
-    def get_total_steps(self):
-        return self.num_epochs * len(self.train_dataloader)
-    
+    def get_total_steps(self, num_epochs):
+        """
+        gets total_steps = num_epochs * (number of training data samples)
+        -----------------------------------------------------------------
+        :param num_epochs:    [int], e.g. 10
+        :return: total_steps: [int], e.g. 2500 (in case of 250 training data samples)
+        """
+        return num_epochs * len(self.train_dataloader)
+
+    ####################################################################################################################
+    # 4. OPTIMIZER
+    ####################################################################################################################
     def clip_grad_norm(self, max_grad_norm):
         torch.nn.utils.clip_grad_norm_(parameters = self.model.parameters(), max_norm=max_grad_norm)
         
@@ -346,7 +409,7 @@ class NERTrainer(object):
             return x/warmup
         return 1.0 - x
     
-    def create_optimizer(self, fp16=True, no_decay=['bias', 'gamma', 'beta']):
+    def create_optimizer(self, learning_rate, fp16=True, no_decay=['bias', 'gamma', 'beta']):
         # Remove unused pooler that otherwise break Apex
         param_optimizer = list(self.model.named_parameters())
         param_optimizer = [n for n in param_optimizer if 'pooler' not in n[0]]
@@ -357,12 +420,12 @@ class NERTrainer(object):
         ]
         if fp16:
             # optimizer = FusedAdam(optimizer_grouped_parameters, lr=self.learning_rate, bias_correction=False)
-            optimizer = AdamW(optimizer_grouped_parameters, lr=self.learning_rate)
+            optimizer = AdamW(optimizer_grouped_parameters, lr=learning_rate)
             # optimizer = FP16_Optimizer(optimizer, dynamic_loss_scale=True)
             
         else:
-            optimizer = Adam(optimizer_grouped_parameters, lr=self.learning_rate)
-            # optimizer = FusedAdam(optimizer_grouped_parameters, lr=self.learning_rate)
+            optimizer = Adam(optimizer_grouped_parameters, lr=learning_rate)
+            # optimizer = FusedAdam(optimizer_grouped_parameters, lr=learning_rate)
         
         # optimizer = BertAdam(optimizer_grouped_parameters,lr=2e-5, warmup=.1)
         return optimizer
