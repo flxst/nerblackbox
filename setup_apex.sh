0f01abaaed269029e569db8e1066b45135a7e52a
Felix Stollenwerk <felix.stollenwerk@arbetsformedlingen.se>
2020-04-16 12:07:20 +0200

    remove setup_apex.sh

--- setup_apex.sh
+++
@@ -1,5 +0,0 @@
-# needs to be installed: sudo apt-get install python3-dev
-export CUDA_HOME=/usr/local/cuda-9.2
-git clone https://github.com/NVIDIA/apex
-cd apex
-pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./
