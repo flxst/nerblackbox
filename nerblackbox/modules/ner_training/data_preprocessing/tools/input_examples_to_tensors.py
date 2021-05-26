8f3c5daa3104582dff689d9427fe705d31233e90
Felix Stollenwerk <felix.stollenwerk@arbetsformedlingen.se>
2021-05-26 11:34:26 +0200

    data preprocessing: use tag_id = 0 instead of -100 for predictions

--- nerblackbox/modules/ner_training/data_preprocessing/tools/input_examples_to_tensors.py
+++ nerblackbox/modules/ner_training/data_preprocessing/tools/input_examples_to_tensors.py
@@ -31,11 +31,14 @@ class InputExamplesToTensors:
             self.default_logger.log_debug("> tag2id:", self.tag2id)
 
     def __call__(self,
-                 input_examples: List[InputExample]) -> Encodings:
+                 input_examples: List[InputExample],
+                 predict: bool) -> Encodings:
         """
         Args:
-            input_examples: List[InputExample], e.g. text = 'at arbetsförmedlingen'
-                                                     tags = '0 ORG'
+            input_examples: List[InputExample] with e.g. text = 'at arbetsförmedlingen'
+                                                         tags = '0 ORG'
+            predict: if True,  map special tokens to    O tag_id for prediction (will not be used)
+                     if False, map special tokens to -100 tag_id for train, val, test
 
         Returns:
             encodings: [Encodings] with
@@ -48,7 +51,7 @@ class InputExamplesToTensors:
         # encodings_single
         encodings_single = {key: list() for key in EncodingsKeys}
         for input_example in input_examples:
-            _encodings = self._transform_input_example(input_example)
+            _encodings = self._transform_input_example(input_example, predict)
             for key in encodings_single.keys():
                 encodings_single[key].append(_encodings[key])
 
@@ -61,13 +64,16 @@ class InputExamplesToTensors:
         return encodings
 
     def _transform_input_example(self,
-                                 input_example: InputExample) -> Encodings:
+                                 input_example: InputExample,
+                                 predict: bool) -> Encodings:
         """
         - transform input_example to tensors of length self.max_seq_length
 
         Args:
-            input_example: [InputExample], e.g. text = 'at arbetsförmedlingen'
-                                                tags = '0 ORG'
+            input_example: [InputExample], with e.g. text = 'at arbetsförmedlingen'
+                                                     tags = '0 ORG'
+            predict: if True,  map special tokens to    O tag_id for prediction (will not be used)
+                     if False, map special tokens to -100 tag_id for train, val, test
 
         Returns:
             encodings: [Encodings] with
@@ -95,7 +101,7 @@ class InputExamplesToTensors:
             return_overflowing_tokens=True,
         )
 
-        encodings["tag_ids"] = self._encode_tags(tags_split_into_words, encodings.offset_mapping)
+        encodings["tag_ids"] = self._encode_tags(tags_split_into_words, encodings.offset_mapping, predict)
         keys = list(encodings.keys())
         for key in keys:
             if key in EncodingsKeys:
@@ -110,15 +116,28 @@ class InputExamplesToTensors:
     ####################################################################################################################
     def _encode_tags(self,
                      _tags_split_into_words: List[str],
-                     all_offsets: List[List[Tuple[int, int]]]) -> List[List[int]]:
+                     all_offsets: List[List[Tuple[int, int]]],
+                     predict: bool) -> List[List[int]]:
+        """
+        Args:
+            _tags_split_into_words: ['at arbetsförmedlingen']
+            all_offsets: [chunks] with chunk = [(start_1, end_1), (start_2, end_2), ..], e.g. [[(0, 2), (2, 5), ..]
+            predict: if True,  map special tokens to    O tag_id for prediction (will not be used)
+                     if False, map special tokens to -100 tag_id for train, val, test
+
+        Returns:
+            all_tag_ids: [chunk_tag_ids] with chunk_tag_ids = e.g. [-100, 3, -100, 4, 5, -100, ..]
+        """
 
         tag_ids_split_into_words: List[int] = [self.tag2id[tag] for tag in _tags_split_into_words]
+
+        tag_id_special = 0 if predict else -100
 
         index = 0
         all_tag_ids = list()
         for offsets in all_offsets:
             # create an empty array of -100
-            arr_tag_ids: np.array = np.ones(len(offsets), dtype=int) * -100
+            arr_tag_ids: np.array = np.ones(len(offsets), dtype=int) * tag_id_special
             arr_offsets: np.array = np.array(offsets)
 
             # set labels whose first offset position is 0 and the second is not 0
