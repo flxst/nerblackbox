5b3c7a01c624cfa2efc560a6063c31339f81cada
Felix Stollenwerk <felix.stollenwerk@ai.se>
2022-12-17 12:30:47 +0100

    Dataset: enable creation from_file & train/val/test split

--- nerblackbox/api/dataset.py
+++ nerblackbox/api/dataset.py
@@ -1,5 +1,10 @@
 import os
+from os.path import isfile
+import json
+from typing import Optional
 from nerblackbox.modules.datasets.formatter.auto_formatter import AutoFormatter
+
+PHASES = ["train", "val", "test"]
 
 
 class Dataset:
@@ -22,6 +27,65 @@ class Dataset:
         """
         self.dataset_name = dataset_name
         self.dataset_subset_name = dataset_subset_name
+        self.file_path = None  # only for instances created through from_file method
+
+    @classmethod
+    def from_file(cls,
+                  dataset_name: str,
+                  file_path: str) -> Optional['Dataset']:
+        r"""
+        create a Dataset instance by reading a file
+
+        Args:
+            dataset_name: name of dataset, e.g. "strangnas"
+            file_path: e.g. "strangnas/strangnas.jsonl"
+
+        Returns:
+            dataset instance
+
+        """
+        if isfile(file_path):
+            print(f"> create Dataset")
+            dataset = Dataset(dataset_name)
+            dataset.file_path = file_path
+            return dataset
+        else:
+            print(f"Error! {file_path} does not exist")
+            return None
+
+    def split(self,
+              val_fraction: float = 0.1,
+              test_fraction: float = 0.2) -> None:
+        r"""
+        - read dataset from self.file_path
+        - split dataset into train/val/test subsets
+        - write subsets to PHASE.jsonl
+
+        Args:
+            val_fraction: e.g. 0.1
+            test_fraction: e.g. 0.2
+        """
+        with open(self.file_path, "r") as f:
+            input_lines = [json.loads(line) for line in f]
+        print(f"> read {len(input_lines)} documents from {self.file_path}")
+
+        index_train_end = (1-val_fraction-test_fraction)*len(input_lines)
+        index_val_end = (1-test_fraction)*len(input_lines)
+        output_lines = {
+            "train": input_lines[:index_train_end],
+            "val": input_lines[index_train_end: index_val_end],
+            "test": input_lines[index_val_end:],
+        }
+
+        print()
+        for phase in PHASES:
+            file_path_phase = "/".join(self.file_path.split("/")[:-1]) + f"/{phase}.jsonl"
+
+            with open(file_path_phase, "w") as f:
+                for line in output_lines[phase]:
+                    f.write(json.dumps(line, ensure_ascii=False) + "\n")
+
+            print(f"> wrote {len(output_lines[phase])} documents to {file_path_phase}")
 
     def set_up(
         self,
